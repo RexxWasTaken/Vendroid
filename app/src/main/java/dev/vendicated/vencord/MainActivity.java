211706aaa8913b2a5c05ee62135a7f241a497f3e
RexxWasTaken <rexxwastak3n@gmail.com>
2026-08-30 15:55:44 +0530

    Update MainActivity.java

--- app/src/main/java/dev/vendicated/vencord/MainActivity.java
+++ app/src/main/java/dev/vendicated/vencord/MainActivity.java
@@ -15,17 +15,18 @@ import java.util.Objects;
 
 public class MainActivity extends Activity {
     public static final int FILECHOOSER_RESULTCODE = 8485;
+
     private boolean wvInitialized = false;
     private WebView wv;
+    private VChromeClient chromeClient;
 
     public ValueCallback<Uri[]> filePathCallback;
 
-    @SuppressLint("SetJavaScriptEnabled") // mad? watch this swag
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // https://developer.chrome.com/docs/devtools/remote-debugging/webviews/
         WebView.setWebContentsDebuggingEnabled(BuildConfig.DEBUG);
 
         setContentView(R.layout.activity_main);
@@ -35,14 +36,29 @@ public class MainActivity extends Activity {
         explodeAndroid();
 
         wv.setWebViewClient(new VWebviewClient());
-        wv.setWebChromeClient(new VChromeClient(this));
+
+        chromeClient = new VChromeClient(this);
+        wv.setWebChromeClient(chromeClient);
 
         var s = wv.getSettings();
+
         s.setJavaScriptEnabled(true);
         s.setDomStorageEnabled(true);
         s.setAllowFileAccess(true);
 
-        wv.addJavascriptInterface(new VencordNative(this, wv), "VencordMobileNative");
+        // Desktop Discord site
+        s.setUserAgentString(Constants.DESKTOP_USER_AGENT);
+
+        s.setUseWideViewPort(true);
+        s.setLoadWithOverviewMode(true);
+
+        // Audio can start without requiring a user gesture
+        s.setMediaPlaybackRequiresUserGesture(false);
+
+        wv.addJavascriptInterface(
+                new VencordNative(this, wv),
+                "VencordMobileNative"
+        );
 
         try {
             HttpClient.fetchVencord(this);
@@ -52,9 +68,13 @@ public class MainActivity extends Activity {
         }
 
         Intent intent = getIntent();
+
         if (Objects.equals(intent.getAction(), Intent.ACTION_VIEW)) {
             Uri data = intent.getData();
-            if (data != null) handleUrl(intent.getData());
+
+            if (data != null)
+                handleUrl(intent.getData());
+
         } else {
             wv.loadUrl("https://discord.com/app");
         }
@@ -65,60 +85,118 @@ public class MainActivity extends Activity {
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         if (keyCode == KeyEvent.KEYCODE_BACK && wv != null) {
-            runOnUiThread(() -> wv.evaluateJavascript("VencordMobile.onBackPress()", r -> {
-                if ("false".equals(r))
-                    this.onBackPressed ();
-            }));
+
+            runOnUiThread(() ->
+                    wv.evaluateJavascript(
+                            "VencordMobile.onBackPress()",
+                            r -> {
+                                if ("false".equals(r))
+                                    this.onBackPressed();
+                            }
+                    )
+            );
+
             return true;
         }
+
         return super.onKeyDown(keyCode, event);
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
-        if (requestCode != FILECHOOSER_RESULTCODE || filePathCallback == null)
+    protected void onActivityResult(
+            int requestCode,
+            int resultCode,
+            Intent intent
+    ) {
+        if (requestCode != FILECHOOSER_RESULTCODE ||
+                filePathCallback == null)
             return;
 
         if (resultCode != RESULT_OK || intent == null) {
+
             filePathCallback.onReceiveValue(null);
+
         } else {
+
             Uri[] uris;
+
             try {
                 var clipData = intent.getClipData();
-                if (clipData != null) { // multiple items
+
+                if (clipData != null) {
+
                     uris = new Uri[clipData.getItemCount()];
+
                     for (int i = 0; i < clipData.getItemCount(); i++) {
                         uris[i] = clipData.getItemAt(i).getUri();
                     }
-                } else { // single item
-                    uris = new Uri[] { intent.getData() };
+
+                } else {
+
+                    uris = new Uri[]{
+                            intent.getData()
+                    };
                 }
+
             } catch (Exception ex) {
+
                 Logger.e("Error during file upload", ex);
                 uris = null;
             }
 
             filePathCallback.onReceiveValue(uris);
         }
+
         filePathCallback = null;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode,
+            String[] permissions,
+            int[] grantResults
+    ) {
+        super.onRequestPermissionsResult(
+                requestCode,
+                permissions,
+                grantResults
+        );
+
+        if (chromeClient != null) {
+            chromeClient.onAndroidPermissionResult(
+                    requestCode,
+                    grantResults
+            );
+        }
     }
 
     private void explodeAndroid() {
         StrictMode.setThreadPolicy(
                 new StrictMode.ThreadPolicy.Builder()
-                        // trolley
                         .permitNetwork()
                         .build()
         );
     }
 
     public void handleUrl(Uri url) {
+
         if (url != null) {
-            if (!url.getAuthority().equals("discord.com")) return;
+
+            if (!url.getAuthority().equals("discord.com"))
+                return;
+
             if (!wvInitialized) {
+
                 wv.loadUrl(url.toString());
+
             } else {
-                wv.evaluateJavascript("Vencord.Webpack.Common.NavigationRouter.transitionTo(\"" + url.getPath() + "\")", null);
+
+                wv.evaluateJavascript(
+                        "Vencord.Webpack.Common.NavigationRouter.transitionTo(\""
+                                + url.getPath()
+                                + "\")",
+                        null
+                );
             }
         }
     }
@@ -126,7 +204,10 @@ public class MainActivity extends Activity {
     @Override
     protected void onNewIntent(Intent intent) {
         super.onNewIntent(intent);
+
         Uri data = intent.getData();
-        if (data != null) handleUrl(data);
+
+        if (data != null)
+            handleUrl(data);
     }
 }
