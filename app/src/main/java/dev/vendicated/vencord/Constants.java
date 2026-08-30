630829a710742ee6660015a288aa9c1cba03eab1
RexxWasTaken <rexxwastak3n@gmail.com>
2026-08-30 15:54:57 +0530

    Update Constants.java

--- app/src/main/java/dev/vendicated/vencord/Constants.java
+++ app/src/main/java/dev/vendicated/vencord/Constants.java
@@ -1,5 +1,11 @@
 package dev.vendicated.vencord;
 
 public class Constants {
-    public static final String JS_BUNDLE_URL = "https://github.com/Vendicated/Vencord/releases/download/devbuild/browser.js";
+    public static final String JS_BUNDLE_URL =
+            "https://github.com/Vendicated/Vencord/releases/download/devbuild/browser.js";
+
+    // Desktop Chrome/Windows User-Agent
+    public static final String DESKTOP_USER_AGENT =
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 " +
+            "(KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36";
 }
