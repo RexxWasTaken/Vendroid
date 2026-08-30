c61f66ec6d2bc65837f421b252a264748ee206b3
RexxWasTaken <rexxwastak3n@gmail.com>
2026-08-30 15:56:52 +0530

    Update VWebviewClient.java

--- app/src/main/java/dev/vendicated/vencord/VWebviewClient.java
+++ app/src/main/java/dev/vendicated/vencord/VWebviewClient.java
@@ -13,64 +13,168 @@ import java.net.URL;
 import java.util.*;
 
 public class VWebviewClient extends WebViewClient {
+
     @Override
-    public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+    public boolean shouldOverrideUrlLoading(
+            WebView view,
+            WebResourceRequest request
+    ) {
+
         var url = request.getUrl();
-        if ("discord.com".equals(url.getAuthority()) || "about:blank".equals(url.toString())) {
+
+        if ("discord.com".equals(url.getAuthority())
+                || "about:blank".equals(url.toString())) {
+
             return false;
         }
 
-        Intent intent = new Intent(Intent.ACTION_VIEW, url);
+        Intent intent = new Intent(
+                Intent.ACTION_VIEW,
+                url
+        );
+
         view.getContext().startActivity(intent);
+
         return true;
     }
 
     @Override
-    public void onPageStarted(WebView view, String url, Bitmap favicon) {
-        view.evaluateJavascript(HttpClient.VencordRuntime, null);
-        view.evaluateJavascript(HttpClient.VencordMobileRuntime, null);
+    public void onPageStarted(
+            WebView view,
+            String url,
+            Bitmap favicon
+    ) {
+
+        // Desktop script FIRST
+        view.evaluateJavascript(
+                HttpClient.VendroidDesktopRuntime,
+                null
+        );
+
+        // Vencord runtime
+        view.evaluateJavascript(
+                HttpClient.VencordRuntime,
+                null
+        );
+
+        // Existing mobile runtime
+        view.evaluateJavascript(
+                HttpClient.VencordMobileRuntime,
+                null
+        );
     }
 
     @Override
-    public void onPageFinished(WebView view, String url) {
+    public void onPageFinished(
+            WebView view,
+            String url
+    ) {
+
         view.setVisibility(View.VISIBLE);
+
+        // Custom plugins load separately.
+        for (String script : HttpClient.CustomPluginScripts) {
+
+            String wrapped =
+                    "(function(){try{\n"
+                            + script
+                            + "\n}catch(e){console.error('[Vendroid custom plugin error]',e);}})();";
+
+            view.evaluateJavascript(
+                    wrapped,
+                    null
+            );
+        }
+
         super.onPageFinished(view, url);
     }
 
     @Nullable
     @Override
-    public WebResourceResponse shouldInterceptRequest(WebView view, WebResourceRequest req) {
+    public WebResourceResponse shouldInterceptRequest(
+            WebView view,
+            WebResourceRequest req
+    ) {
+
         var uri = req.getUrl();
-        if (req.isForMainFrame() || req.getUrl().getPath().endsWith(".css")) {
+
+        if (req.isForMainFrame()
+                || req.getUrl().getPath().endsWith(".css")) {
+
             try {
+
                 return doFetch(req);
+
             } catch (IOException ex) {
-                Logger.e("Error during shouldInterceptRequest", ex);
+
+                Logger.e(
+                        "Error during shouldInterceptRequest",
+                        ex
+                );
             }
         }
+
         return null;
     }
 
-    private WebResourceResponse doFetch(WebResourceRequest req) throws IOException {
+    private WebResourceResponse doFetch(
+            WebResourceRequest req
+    ) throws IOException {
+
         var url = req.getUrl().toString();
-        var conn = (HttpURLConnection) new URL(url).openConnection();
+
+        var conn =
+                (HttpURLConnection)
+                        new URL(url).openConnection();
+
         conn.setRequestMethod(req.getMethod());
+
         for (var h : req.getRequestHeaders().entrySet()) {
-            conn.setRequestProperty(h.getKey(), h.getValue());
+
+            conn.setRequestProperty(
+                    h.getKey(),
+                    h.getValue()
+            );
         }
 
         var code = conn.getResponseCode();
         var msg = conn.getResponseMessage();
 
         var headers = conn.getHeaderFields();
-        var modifiedHeaders = new HashMap<String, String>(headers.size());
+
+        var modifiedHeaders =
+                new HashMap<String, String>(
+                        headers.size()
+                );
+
         for (var header : headers.entrySet()) {
-            if (!"Content-Security-Policy".equalsIgnoreCase(header.getKey())) {
-                modifiedHeaders.put(header.getKey(), header.getValue().get(0));
+
+            if (!"Content-Security-Policy"
+                    .equalsIgnoreCase(header.getKey())) {
+
+                modifiedHeaders.put(
+                        header.getKey(),
+                        header.getValue().get(0)
+                );
             }
         }
-        if (url.endsWith(".css")) modifiedHeaders.put("Content-Type", "text/css");
 
-        return new WebResourceResponse(modifiedHeaders.getOrDefault("Content-Type", "application/octet-stream"), "utf-8", code, msg, modifiedHeaders, conn.getInputStream());
+        if (url.endsWith(".css"))
+            modifiedHeaders.put(
+                    "Content-Type",
+                    "text/css"
+            );
+
+        return new WebResourceResponse(
+                modifiedHeaders.getOrDefault(
+                        "Content-Type",
+                        "application/octet-stream"
+                ),
+                "utf-8",
+                code,
+                msg,
+                modifiedHeaders,
+                conn.getInputStream()
+        );
     }
 }
